014ab8aaec1e713ae8ff094a464338d29f70001e
wishwa-50 <jayanathvishva6@gmail.com>
2024-09-01 08:07:59 +0530

    just leveling local and remote

--- classroom/settings.py
+++ classroom/settings.py
@@ -139,6 +139,9 @@ LOGOUT_REDIRECT_URL = '/account/login'
 
 STATIC_URL = 'static/'
 
+# Define STATIC_ROOT for collecting static files in production
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
