ed0c4357d75318f397fe89fd559720071855b1bb
wishwa-50 <jayanathvishva6@gmail.com>
2024-09-01 08:56:17 +0530

    added classrom to installed apps and changed the path to find ssl file correctly

--- classroom/management/commands/runsslserver.py
+++ classroom/management/commands/runsslserver.py
@@ -1,12 +1,18 @@
 from django.core.management.commands.runserver import Command as RunserverCommand
 import ssl
+import os
 
 class Command(RunserverCommand):
+    
+    help = 'Run Django development server with SSL support'
+    
     def get_handler(self, *args, **options):
         handler = super().get_handler(*args, **options)
         
-        cert_file = 'ssl/cert.pem'
-        key_file = 'ssl/key.pem'
+        base_dir = os.path.dirname(os.path.abspath(__file__))
+        cert_file = os.path.join(base_dir, '../../ssl/cert.pem')
+        key_file = os.path.join(base_dir, '../../ssl/key.pem')
+        
         
         ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
         ssl_context.load_cert_chain(certfile=cert_file, keyfile=key_file)
--- classroom/settings.py
+++ classroom/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'channels',
     'videochat',
+    'classroom'
     
     
 ]
