b3a479ec7a3a88ea0152d48df85230ecdec9e26d
wishwaj129 <60093220+Wishwa-code@users.noreply.github.com>
2025-03-15 15:16:27 +0530

    Update views.py

--- classroom/views.py
+++ classroom/views.py
@@ -6,7 +6,7 @@ from django.views import generic
 
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
-    success_url = reverse_lazy('login')
+    success_url = reverse_lazy('accounts/login/')
     template_name = 'registration/signup.html'
 
 @login_required
