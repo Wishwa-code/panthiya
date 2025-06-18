f52baca5ecaa6623da4bf323b17cebd50fb55776
Wishwa-code <wishwakankanamge129@gmail.com>
2025-06-18 17:48:18 +0530

    add signal to automatically create profile  when  usr object is created

--- videochat/models.py
+++ videochat/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 class Profile(models.Model):
@@ -42,3 +44,13 @@ class Classrooms(models.Model):
             "timestamp": self.timestamp.strftime("%b %d %Y, %I:%M %p"),
             "image_url": self.thumbnail.url if self.thumbnail else None
         }
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    if hasattr(instance, 'profile'):
+        instance.profile.save()
