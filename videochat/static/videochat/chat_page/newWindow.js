379f60bf6928c8a9352fee7ee6cce9599d0ca6c6
Wishwa-code <wishwakankanamge129@gmail.com>
2025-06-24 08:22:57 +0530

    removed unnecesssary commennts

--- videochat/static/videochat/chat_page/newWindow.js
+++ videochat/static/videochat/chat_page/newWindow.js
@@ -1,19 +1,15 @@
-// NewWindow.js - Create this new file
 
 function NewWindow(props) {
     const [container, setContainer] = React.useState(null);
     const newWindow = React.useRef(null);
 
     React.useEffect(() => {
-        // Open a new window and store a reference to it
         newWindow.current = window.open('', 'callWindow', 'width=700,height=600');
 
-        // Create a container div in the new window to render into
         const div = newWindow.current.document.createElement('div');
         newWindow.current.document.body.appendChild(div);
         setContainer(div);
         
-        // Copy all stylesheets from the main page to the new window
         Array.from(document.styleSheets).forEach(styleSheet => {
             if (styleSheet.href) {
                 const newLinkEl = newWindow.current.document.createElement('link');
@@ -29,10 +25,8 @@ function NewWindow(props) {
             }
         });
 
-        // Set a title for the new window
         newWindow.current.document.title = "Video Call";
 
-        // Call the onUnload prop when the popup is closed
         const handleUnload = () => {
             if (props.onUnload) {
                 props.onUnload();
@@ -40,13 +34,11 @@ function NewWindow(props) {
         };
         newWindow.current.addEventListener('beforeunload', handleUnload);
 
-        // Cleanup: close the window when the main component unmounts
         return () => {
             handleUnload();
             newWindow.current.close();
         };
     }, []);
 
-    // Render the children (Sender or Receiver component) into the new window
     return container ? ReactDOM.createPortal(props.children, container) : null;
 }
